197a71ca0c8dd52742509f99b5d7871945ba599d
Ibrahim Bin Mohamed Farid <ibrahimmdfarid@gmail.com>
2024-11-12 15:22:42 +0800

    Update adminpage.php

--- adminpage.php
+++ adminpage.php
@@ -107,14 +107,6 @@ $conn->close();
         display: none; /* Hide by default */
     }
 </style>
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Page</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
 <body>
 
 <header>
