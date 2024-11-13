9edb9d4e21e5a89f6f8f93aa79686ba655242a66
TZJ4444 <tey.zijian@gmail.com>
2024-11-13 23:14:37 +0800

    added missing dropdown

--- view_orders.php
+++ view_orders.php
@@ -302,6 +302,27 @@ $orderResult = $conn->query($orderQuery);
     <p><a href="contactpage.php">Contact Us!</a></p>
 </footer>
 
+<script>
+    // Toggle dropdown visibility
+    function toggleDropdown() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    // Close dropdown if clicked outside
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            const dropdowns = document.getElementsByClassName("dropdown-content");
+            for (let i = 0; i < dropdowns.length; i++) {
+                const openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    };
+
+</script>
+
 </body>
 </html>
 
